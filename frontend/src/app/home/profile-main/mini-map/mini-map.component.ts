2b5d28fe7c50f6259ff316d8190652cb5074890f
janusz.jaskowicz <janusz_jaskowicz@yahoo.com>
2024-11-05 20:36:36 +0100

    additional tweaks for google map

--- frontend/src/app/home/profile-main/mini-map/mini-map.component.ts
+++ frontend/src/app/home/profile-main/mini-map/mini-map.component.ts
@@ -1,21 +1,12 @@
-import {
-  Component,
-  Input,
-  Output,
-  EventEmitter,
-  ViewChild,
-} from '@angular/core';
-import {
-  GoogleMapsModule,
-  MapInfoWindow,
-  MapMarker,
-  GoogleMap,
-} from '@angular/google-maps';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { CommonModule } from '@angular/common';
+import { Component, Input, ViewChild } from '@angular/core';
+import { GoogleMapsModule, MapMarker, GoogleMap } from '@angular/google-maps';
 
 @Component({
   standalone: true,
   selector: 'app-mini-map',
-  imports: [GoogleMapsModule],
+  imports: [CommonModule, GoogleMapsModule, MatProgressSpinnerModule],
   templateUrl: './mini-map.component.html',
   styleUrls: ['./mini-map.component.scss'],
 })
@@ -24,6 +15,8 @@ export class MiniMapComponent {
   @Input() latitude!: number;
   @Input() longitude!: number;
 
+  initMap: boolean = false;
+
   center: google.maps.LatLngLiteral = {
     lat: 52.069167,
     lng: 19.480278,
@@ -31,6 +24,7 @@ export class MiniMapComponent {
   zoom = 14;
 
   ngOnInit(): void {
+    setTimeout(() => (this.initMap = true), 500);
     this.center = {
       lat: this.latitude || 52.069167,
       lng: this.longitude || 19.480278,
